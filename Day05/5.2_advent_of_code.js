ba8e5da0c514612c216e99e9e9ef5bfb3c2671cf
Markus Warne <markus.warne@gmail.com>
2017-12-05 19:07:11 +0100

    Solutions for day 5

--- Day05/5.2_advent_of_code.js
+++ Day05/5.2_advent_of_code.js
@@ -4,11 +4,16 @@ const moves = input.split('\n').map(x => parseInt(x));
 const steps = [];
 let idx = 0;
 
-while(idx <= (moves.length - 1)){
+while(idx < moves.length){
   const nextStep = moves[idx];
   steps.push(nextStep);
-  moves[idx]++;
+  if (nextStep >= 3) {
+    moves[idx]--;
+  } else {
+    moves[idx]++;
+  }
   idx += nextStep;
 }
 
 console.log(steps.length);
+
