7b0938082d87a4c53676d247b762e38d1e0b1512
Markus Warne <markus.warne@gmail.com>
2017-12-06 20:17:24 +0100

    Solution for day 6

---
+++ Day06/6.2_advent_of_code.js
@@ -0,0 +1,37 @@
+const { input } = require('./input');
+let memBanks = input.split('\t').map(x => parseInt(x));
+
+const previous = [];
+
+const getMaxData = array => {
+  const max = Math.max(...array);
+  return {
+    max, 
+    idx: array.indexOf(max),
+  };
+};
+
+const arraysAreEqual = (arr1, arr2) => arr1.every((val, i) => val === arr2[i]);
+let loopStartIdx;
+
+ do {
+  previous.push([...memBanks]);
+  const maxData = getMaxData(memBanks);
+  let redistAmount = maxData.max
+  const diffArr = Array(memBanks.length).fill(0);
+  for (let i = (maxData.idx+1) % memBanks.length; redistAmount > 0; redistAmount--){
+    diffArr[i]++;
+    i = (i + 1) % memBanks.length;
+  }
+  memBanks[maxData.idx] = 0;
+  memBanks = memBanks.map((val, i) => val + diffArr[i]);
+  
+  if(previous.some(prevRes => arraysAreEqual(prevRes, memBanks))) {
+    loopStartIdx = previous.findIndex(arr => arraysAreEqual(arr, memBanks));
+  }
+} while (!previous.some(prevRes => arraysAreEqual(prevRes, memBanks)))
+
+
+console.log('loop length: ', previous.length - loopStartIdx);
+
+
