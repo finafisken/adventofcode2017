49e5985d0deea6e0f0b45090f29899527e421168
Markus Warne <markus.warne@gmail.com>
2017-12-06 19:49:30 +0100

    Solution for day 6

--- Day06/6.1_advent_of_code.js
+++ Day06/6.1_advent_of_code.js
@@ -1,11 +1,33 @@
 const { input } = require('./input');
-const memBanks = input.split('\t').map(x => parseInt(x));
-console.log(memBanks);
-console.log(memBanks.length);
+let memBanks = input.split('\t').map(x => parseInt(x));
 
-const previous = [
-  [...memBanks]
-];
+const previous = [];
+
+const getMaxData = array => {
+  const max = Math.max(...array);
+  return {
+    max, 
+    idx: array.indexOf(max),
+  };
+};
+
+const arraysAreEqual = (arr1, arr2) => arr1.every((val, i) => val === arr2[i]);
+
+ do {
+  previous.push([...memBanks]);
+  const maxData = getMaxData(memBanks);
+  let redistAmount = maxData.max
+  const diffArr = Array(memBanks.length).fill(0);
+  for (let i = (maxData.idx+1) % memBanks.length; redistAmount > 0; redistAmount--){
+    diffArr[i]++;
+    i = (i + 1) % memBanks.length;
+  }
+  memBanks[maxData.idx] = 0;
+  memBanks = memBanks.map((val, i) => val + diffArr[i]);
+  
+} while (!previous.some(prevRes => arraysAreEqual(prevRes, memBanks)))
+
+console.log('cycles: ', previous.length);
 
 // find max
 // divide by membanks.length (as well as we can)
