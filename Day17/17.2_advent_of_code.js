e9ac0b3cd55850ed90f2706f6eae3d04d8ba3c14
Markus Warne <markus.warne@gmail.com>
2017-12-17 18:33:19 +0100

    Day 17 part 2

---
+++ Day17/17.2_advent_of_code.js
@@ -0,0 +1,19 @@
+const steps = 348;
+
+// seems like 0 will always be first
+// so we can avoid creating the whole list
+// we are only interested in the value after 0
+
+let pos = 0;
+let len = 1;
+let target;
+
+const loop = pos => pos % len;
+
+for (let val = 1; val <= 50000000; val++) {
+  pos = loop((pos + steps)) + 1
+  if(pos === 1) target = val;
+  len++;
+}
+
+console.log(target);
