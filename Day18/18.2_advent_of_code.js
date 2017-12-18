a44872850c0a9ab0c417d2630a8da0fbd6df6091
Markus Warne <markus.warne@viaplay.com>
2017-12-18 18:03:18 +0100

    WIP

--- Day18/18.2_advent_of_code.js
+++ Day18/18.2_advent_of_code.js
@@ -10,30 +10,31 @@ const instructions = input.split("\n").map(row =>
   )
 );
 
-const getVal = val => Number.isInteger(parseInt(val)) ? parseInt(val) : parseInt(registers[val]);
+const getVal = val => Number.isInteger(parseInt(val)) ? parseInt(val) : parseInt(registers[val] || 0);
 const registers = {};
-const sounds = [];
 const fn = {
-  snd: (reg) => sounds.push({ register: reg, sound: registers[reg]}),
+  snd: (reg, val) => {
+    ++sends;
+    ++count;
+  },
   set: (reg, val) => registers[reg] = getVal(val),
   add: (reg, val) => registers[reg] = registers.hasOwnProperty(reg) ? registers[reg] + getVal(val) : getVal(val),
   mul: (reg, val) => registers[reg] = registers.hasOwnProperty(reg) ? registers[reg] * getVal(val) : 0,
   mod: (reg, val) => registers[reg] = registers.hasOwnProperty(reg) ? registers[reg] % getVal(val) : 0,
-  rcv: (reg, val) => val > 0 && sounds.length > 0 && sounds[sounds.length - 1],
-  jgz: (reg, val, cIdx) => (cIdx + val) < instructions.length && (cIdx + val) >= 0 && (cIdx + val),
+  rcv: (reg, val) => --count,
+  jgz: (reg, val, cIdx) => reg > 0 ? ((cIdx + val) < instructions.length && (cIdx + val) >= 0 && (cIdx + val)) : cIdx + 1,
 }
 
+let count = 0;
+let sends = 0;
 const doStep = i => {
-  if (i === instructions.length - 1) return;
+  if (i === instructions.length - 1 || count < 0 || i === false) return sends;
   const cInst = instructions[i];
   const { operation, register, modifier } = cInst;
-  if (operation === 'rcv' && registers[register] > 0) return sounds[sounds.length-1];
+  console.log(i, count, instructions[i].operation, register, registers[register], instructions[i].modifier);
   switch (operation) {
-    case 'snd':
-      fn[operation](register);
-      break;
     case 'jgz':
-      return registers[register] ? doStep(fn[operation](register, getVal(modifier), i)) : doStep(++i);
+      return doStep(fn[operation](getVal(register), getVal(modifier), i));
     default:
       fn[operation](register, modifier);
       break;
@@ -42,3 +43,7 @@ const doStep = i => {
 }
 
 console.log(doStep(0));
+// console.log(instructions);
+
+// 254 too low
+// 255 too low
