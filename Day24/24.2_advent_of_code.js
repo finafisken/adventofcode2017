5784f800b525413ae5c89914521ca6ed6504a94d
Markus Warne <markus.warne@gmail.com>
2018-01-05 15:56:44 +0100

    Day 24 part 2

---
+++ Day24/24.2_advent_of_code.js
@@ -0,0 +1,44 @@
+const { input } = require('./input');
+
+const components = input
+  .split('\n')
+  .map(comp => [parseInt(comp.split('/')[0]), parseInt(comp.split('/')[1])]);
+
+const sum = arr => arr.reduce((a, b) => a + b, 0);
+
+const findMatches = (toMatch, pieces) =>
+  pieces.filter(comp => comp.includes(toMatch));
+
+const longestBridges = [{ length: 67, strength: 0 }];
+
+const buildBridge = (bridge, remaining) => {
+  const matched = findMatches(bridge[bridge.length - 1], remaining);
+
+  if (matched.length === 0) {
+    if(longestBridges[0].length <= bridge.length){
+      longestBridges.push({
+        length: bridge.length,
+        strength: sum(bridge)
+      });
+    }
+    return; // end of tree
+  }
+  matched.forEach(match => {
+    const localBridge = [...bridge];
+    const localRemaining = [...remaining];
+    const rIdx = remaining.findIndex(
+      pair => pair[0] === match[0] && pair[1] === match[1]
+    );
+    localRemaining.splice(rIdx, 1);
+    match =
+      match.indexOf(localBridge[localBridge.length - 1]) === 1
+        ? match.reverse()
+        : match;
+    localBridge.push(...match);
+    return buildBridge(localBridge, localRemaining);
+  });
+};
+
+buildBridge([0], components);
+
+console.log(JSON.stringify(longestBridges, null, 2));
